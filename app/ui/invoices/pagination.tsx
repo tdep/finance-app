fd5aa6f11f7d199d067d96f3a618a73c3e127235
Trevor DePew <depet@MackIsWhack.local>
2024-08-13 16:20:58 -0400

    adds pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,13 +4,23 @@ import { ArrowLeftIcon, ArrowRightIcon} from "@heroicons/react/24/outline";
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from "@/app/lib/utils";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function Pagination({ totalPages }: {totalPages: number}) {
-    // const allPages = generatePagination(currentPage, totalPages);
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+    const currentPage = Number(searchParams.get('page')) || 1;
+    const allPages = generatePagination(currentPage, totalPages);
+
+    const createPageUrl = (pageNumber: number | string) => {
+        const params = new URLSearchParams(searchParams);
+        params.set('page', pageNumber.toString());
+        return `${pathname}?${params.toString()}`;
+    };
 
     return (
         <>
-        {/* <div className={"inline-flex"}>
+        <div className={"inline-flex"}>
                 <PaginationArrow
                     direction={"left"}
                     href={createPageUrl(currentPage - 1)}
@@ -43,7 +53,7 @@ export default function Pagination({ totalPages }: {totalPages: number}) {
                     href={createPageUrl(currentPage + 1)}
                     isDisabled={currentPage >= totalPages}
                 />
-            </div> */}
+            </div>
         </>
     )
 }
