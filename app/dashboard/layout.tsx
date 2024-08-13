8607408406b96f147497e3962e290a2b954f8e3b
Trevor DePew <depet@MackIsWhack.local>
2024-08-13 15:22:06 -0400

    adds invoice table, page, and pagination

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,16 @@
 import SideNav from "@/app/ui/dashboard/sideNav";
 
+{/* PPR (Partial Prerendering) https://nextjs.org/learn/dashboard-app/partial-prerendering
+1. Install latest Next.js Canary version:
+    `npm install next@canary`
+2. Add the following in next.config.mjs within the curly braces:
+    experimental: {
+        ppr: 'incremental',
+    },
+3. Uncomment from here:
+    export const experimental_ppr = true; // requires Next.js canary version
+*/}
+
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className={"flex h-screen flex-col md:flex-row md:overflow-hidden"}>
